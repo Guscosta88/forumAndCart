4a2862eccc46aca8f4a2059b159f0976b1412df1
Guscosta88 <guslfcosta@gmail.com>
2021-06-18 10:28:51 +1000

    dbs

--- ch21_include.php
+++ ch21_include.php
@@ -3,8 +3,8 @@ function doDB() {
 global $mysqli;
 
 //connect to server and select database; you may need it
-$mysqli = mysqli_connect("localhost", "root",
-"", "forumrecords");
+$mysqli = mysqli_connect("localhost", "ba3f14a466d6d0",
+"9457db6d", "heroku_d7a77ab923d0ff8");
 
 //if connection fails, stop script execution
  if (mysqli_connect_errno()) {
--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli("localhost","root","","cart_system");
+	$conn = new mysqli("localhost","b69cf2b2d42fd7","ff4b8b03","heroku_65a889a6296ff12");
 	if($conn->connect_error){
 		die("Connection Failed!".$conn->connect_error);
 	}
